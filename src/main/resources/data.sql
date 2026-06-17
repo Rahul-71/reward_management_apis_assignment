563c3a660d5a00ea86d48a0728fe0e3e83120a5e
Rahul-71 <rahultiwari.201307@gmail.com>
2026-06-17 13:44:09 +0530

    add dummy data for local testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO customers (name, email) VALUES ('Clark', 'clark@gmail.com');
+INSERT INTO customers (name, email) VALUES ('Kartos', 'kartos@gmail.com');
+INSERT INTO customers (name, email) VALUES ('Harry', 'harry@gmail.com');
+
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (1, 120.00, '2026-04-05 10:00:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (1, 75.50,  '2026-04-18 14:30:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (1, 200.00, '2026-05-10 09:15:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (1, 45.00,  '2026-05-22 16:45:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (1, 150.75, '2026-06-03 11:20:00');
+
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (2, 35.00,  '2026-04-08 13:00:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (2, 90.00,  '2026-04-20 17:00:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (2, 110.00, '2026-05-15 10:30:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (2, 60.00,  '2026-05-28 12:00:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (2, 150.00, '2026-06-10 08:45:00');
+
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (3, 55.00,  '2026-04-12 15:30:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (3, 250.00, '2026-05-20 14:00:00');
+INSERT INTO transaction_records (customer_id, amount, transaction_date) VALUES (3, 80.00,  '2026-06-08 09:00:00');
