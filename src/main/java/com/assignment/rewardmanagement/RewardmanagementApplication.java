50d9cfd1a09fc1c1cf8005c8551be2d60b65b759
Rahul-71 <rahultiwari.201307@gmail.com>
2026-06-17 08:07:18 +0530

    initial project structure

---
+++ src/main/java/com/assignment/rewardmanagement/RewardmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.assignment.rewardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardmanagementApplication.class, args);
+    }
+}
